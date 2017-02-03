22ff848d1868127797dfb0e6ee904d49ea65b1cf
Chiara DeLucia <chiaradelucia@Chiaras-MacBook-Pro.local>
2017-02-03 16:57:42 -0500

    Adding gitignore and twitter keys.

---
+++ assets/javascript/keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'o4LgiFUFhPa5vSvcqlUbaAuQ5',
+  consumer_secret: 'kxM2wJ3PaC8NstFtmwq4IKjGIkaZ72yWHacf4LrCTWTAeyRTyp',
+  access_token_key: '66230936-m5xOdWH8kpSqMv7XS01I7iycl4VrZoWQxIOdR2e1c',
+  access_token_secret: 'xGTWr8MNVKzPsUuHizTGBndZ9IyVjnFhszDFpG0xe9Qpw'
+};
---
+++ assets/javascript/liri.js
@@ -0,0 +1,11 @@
+
+var keys = require('./keys.js')
+
+// console.log(keys);
+// console.log(keys.twitterKeys);
+
+for (var k in keys.twitterKeys) {
+	var index = keys.twitterKeys[k].split(',');
+	
+};
+
